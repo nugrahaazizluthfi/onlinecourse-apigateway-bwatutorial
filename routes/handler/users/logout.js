196336ee68d514f383ab5d3250b3ad3ef7539305
Luthfi Aziz Nugraha <nugrahaazizluthfi@yahoo.com>
2020-12-04 10:15:22 +0700

    implementasi jwt & integrasi api-gateway

---
+++ routes/handler/users/logout.js
@@ -0,0 +1,21 @@
+const apiAdapter = require("../../../utils/apiadapter");
+const { APP_URL_SERVICE_USER } = process.env;
+
+const api = apiAdapter(APP_URL_SERVICE_USER);
+
+module.exports = async (req, res) => {
+  try {
+    const id = req.user.data.id;
+    const user = await api.post(`/users/logout`, { user_id: id });
+    return res.json(user.data);
+  } catch (error) {
+    if (error.code === "ECONNREFUSED") {
+      return res
+        .status(500)
+        .json({ status: "error", message: "service unvailable" });
+    }
+
+    const { status, data } = error.response;
+    return res.status(status).json(data);
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 const userHandler = require("./handler/users");
+const verifyToken = require("../middleware/verifyToken");
 
 /* GET home page. */
 router.post("/register", userHandler.register);
 router.post("/login", userHandler.login);
+router.put("/", verifyToken, userHandler.update);
+router.get("/", verifyToken, userHandler.getUser);
+router.post("/logout", verifyToken, userHandler.logout);
 
 module.exports = router;
